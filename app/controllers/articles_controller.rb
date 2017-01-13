c325f5fa8b5e6684e83a7ec98ca6ac205d80ae9b
davygora <davygora.e@gmail.com>
2017-01-13 15:22:20 +0200

    some refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.paginate(:page => params[:page], :per_page => 10)
+    @articles = Article.paginate(page: params[:page], per_page: 10)
   end
 end
